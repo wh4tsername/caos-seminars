2096edd4915d61048a82d064442b4e34ceb4bdb5
Denis Pominov <pominov.denis@mail.ru>
2022-03-04 15:55:55 +0300

    format

--- sem18/task0.c
+++ sem18/task0.c
@@ -1,27 +1,28 @@
 #include <pthread.h>
 #include <stdio.h>
 
-void* reverse_numbers(void* arg) {
-  int value = 0;
-  if (scanf("%d", &value) != 1) {
+void* reverse_numbers(void* arg)
+{
+    int value = 0;
+    if (scanf("%d", &value) != 1) {
+        return NULL;
+    }
+
+    pthread_t next_thread;
+    pthread_create(&next_thread, NULL, reverse_numbers, NULL);
+
+    pthread_join(next_thread, NULL);
+    printf("%d\n", value);
+
     return NULL;
-  }
-
-  pthread_t next_thread;
-  pthread_create(&next_thread, NULL, reverse_numbers, NULL);
-
-  pthread_join(next_thread, NULL);
-  printf("%d\n", value);
-
-  return NULL;
 }
 
-int main() {
-  pthread_t init_thread;
+int main()
+{
+    pthread_t init_thread;
 
-  pthread_create(&init_thread, NULL, reverse_numbers, NULL);
-  pthread_join(init_thread, NULL);
+    pthread_create(&init_thread, NULL, reverse_numbers, NULL);
+    pthread_join(init_thread, NULL);
 
-  return 0;
+    return 0;
 }
-
