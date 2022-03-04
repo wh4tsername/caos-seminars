110825fcb1a399744d5aa492edac3619cb061676
Denis Pominov <pominov.denis@mail.ru>
2022-03-04 15:39:06 +0300

    sem18 added

---
+++ sem18/task0.c
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void* reverse_numbers(void* arg) {
+  int value = 0;
+  if (scanf("%d", &value) != 1) {
+    return NULL;
+  }
+
+  pthread_t next_thread;
+  pthread_create(&next_thread, NULL, reverse_numbers, NULL);
+
+  pthread_join(next_thread, NULL);
+  printf("%d\n", value);
+
+  return NULL;
+}
+
+int main() {
+  pthread_t init_thread;
+
+  pthread_create(&init_thread, NULL, reverse_numbers, NULL);
+  pthread_join(init_thread, NULL);
+
+  return 0;
+}
+
